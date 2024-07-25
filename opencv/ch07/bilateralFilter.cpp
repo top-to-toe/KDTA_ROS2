945cab5a9224575181cc63f32ad78396ef6e36f2
Hansol-Lee <hansollee333@gmail.com>
2024-07-25 17:20:39 +0900

    openCV 3rd Day

---
+++ opencv/ch07/bilateralFilter.cpp
@@ -0,0 +1,27 @@
+#include "opencv2/opencv.hpp"
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+String folder = "/home/hansollee/Documents/KDTA_ROS2/opencv/data/";
+
+int main() {
+    Mat src = imread(folder + "lenna.bmp", IMREAD_GRAYSCALE);
+    Mat noise(src.size(), CV_32FC1);
+    int stddev = 10;
+    randn(noise, 0, stddev);
+
+    // src = src + noise;
+    add(src, noise, src, Mat(), CV_8U);
+
+    Mat dst1, dst2;
+    GaussianBlur(src, dst1, Size(), 5);
+    bilateralFilter(src, dst2, -1, 10, 5);
+
+    imshow("src", src);
+    imshow("dst1(Gaussian)", dst1);
+    imshow("dst2(Bilateral)", dst2);
+
+    waitKey(0);
+    return 0;
+}
