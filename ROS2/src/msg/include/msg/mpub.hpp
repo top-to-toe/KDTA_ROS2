75f315b9c2134162b704fd7040efad5e3affd909
Hansol Lee <hansollee333@gmail.com>
2024-08-01 01:09:38 +0900

    　

---
+++ ROS2/src/msg/include/msg/mpub.hpp
@@ -0,0 +1,23 @@
+#ifndef MPUB_HPP
+#define MPUB_HPP
+
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+#include <chrono>
+
+using namespace std;
+using namespace std::chrono_literals;
+
+class MessagePublisher : public rclcpp::Node {
+    public: 
+        MessagePublisher();
+
+    private: 
+        int _i;
+        rclcpp::Publisher<std_msgs::msg::String>::SharedPtr _pub1;
+        rclcpp::Publisher<std_msgs::msg::String>::SharedPtr _pub2;
+        rclcpp::TimerBase::SharedPtr _timer;
+        void publish_message_msg();
+};
+
+#endif
---
+++ ROS2/src/msg/include/msg/tpub.hpp
@@ -0,0 +1,24 @@
+#ifndef TPUB_HPP
+#define TPUB_HPP
+
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/header.hpp"
+#include <iostream>
+#include <chrono>
+
+using namespace std;
+using namespace std::chrono_literals;
+
+class TimePublisher : public rclcpp::Node {
+    public: 
+        TimePublisher();
+
+    private: 
+        int _i;
+        rclcpp::Publisher<std_msgs::msg::Header>::SharedPtr _pub;
+        rclcpp::TimerBase::SharedPtr _timer;
+        rclcpp::Clock _clock;
+        void publish_time_msg();
+};
+
+#endif
